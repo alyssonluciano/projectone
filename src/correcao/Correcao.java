351d21eaff2780e203f99f4db649f486ea6e8dbc
Alysson <Alysson@cpd04-PC>
2017-05-08 16:15:55 -0300

    Primeiro Commit

---
+++ src/correcao/Correcao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correcao;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alysson
+ */
+public class Correcao {
+
+    /**
+     * @param args the command line arguments
+     */
+    static String disciplina;
+    int questoes, acertos;
+    double valor;
+    static double resultado;
+
+    public static void main(String[] args) {
+
+        Correcao corrigir = new Correcao();
+        corrigir.conferencia();
+    }
+
+    public void conferencia() {
+        disciplina = JOptionPane.showInputDialog(null, "Nome da Disciplina");
+        questoes = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantidade de questões"));
+        acertos = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero de acertos"));
+
+        valor = 10.0 / questoes;
+        resultado = (valor * acertos);
+
+        int dialogButton = JOptionPane.showConfirmDialog(null, "A nota em " + disciplina + " : " + resultado,
+                "Corrigir novamente ?", JOptionPane.YES_NO_OPTION);
+        if (dialogButton == JOptionPane.YES_OPTION) {
+
+            Correcao corrigir = new Correcao();
+            corrigir.conferencia();
+            resultado = 0;
+            disciplina = "";
+
+        } else {
+            System.exit(0);
+        }
+
+    }
+}
