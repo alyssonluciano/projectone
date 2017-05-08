263255011574abe0e51d62b2f07e11f5c2de69fc
alyssonluciano <rmareparo@gmail.com>
2017-05-08 16:37:05 -0300

    Retirado o nome git

--- src/correcao/Correcao.java
+++ src/correcao/Correcao.java
@@ -20,7 +20,7 @@ public class Correcao {
     int questoes, acertos;
     double valor;
     static double resultado;
-//teste git
+//teste
     public static void main(String[] args) {
 
         Correcao corrigir = new Correcao();
